3d31378c4090a5495058eea24b410600c2d525ad
javierCueto <javelie@outlook.com>
2020-11-28 13:02:39 -0700

    Intro Auto Layout Landscape with Multipliers - Using custom text

--- AutolayoutUIKit/ViewController.swift
+++ AutolayoutUIKit/ViewController.swift
@@ -12,13 +12,27 @@ class ViewController: UIViewController {
     let bearImageView: UIImageView = {
         let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
     let descriptionTextView: UITextView = {
         let textView = UITextView()
-        textView.text = "Join us today in our fun and games!"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+        
+        attributedText.append(NSMutableAttributedString(string: """
+
+
+
+    Are you ready for loads and loads of fun?
+Don't wait any longer! We hope to se you in
+our stores soon.
+""", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13),NSAttributedString.Key.foregroundColor: UIColor.gray]))
+        
+        textView.attributedText = attributedText
+        
+        //textView.text = ""
+        //textView.font = UIFont.boldSystemFont(ofSize: 18)
         textView.textAlignment = .center
         textView.isEditable = false
         textView.isScrollEnabled = false
@@ -29,23 +43,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(bearImageView)
+       // view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
         
-        setipLaoyut()
+        setupLaoyut()
         
     }
     
-    private func setipLaoyut(){
+    private func setupLaoyut(){
+        let topImageContainerView = UIView()
+       // topImageContainerView.backgroundColor = .blue
+        view.addSubview(topImageContainerView)
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
-        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        bearImageView.topAnchor.constraint(equalTo: view.topAnchor,constant: 100).isActive = true
-        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        /*
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true*/
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor,multiplier: 0.5).isActive = true
         
-        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 150).isActive = true
-        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topImageContainerView.addSubview(bearImageView)
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+        
+        
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor,constant: 24).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
 
