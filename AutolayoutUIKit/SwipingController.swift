829440bba5cc5cf8b2d094878d63210938db7bdb
javierCueto <javelie@outlook.com>
2020-12-02 20:18:54 -0700

    Swift: Autolayout Controls For Swiping/Paging

--- AutolayoutUIKit/SwipingController.swift
+++ AutolayoutUIKit/SwipingController.swift
@@ -44,12 +44,72 @@ class SwipingCOntroller: UICollectionViewController, UICollectionViewDelegateFlo
     """)
     ]
     
+    //
+    private let previusButton: UIButton = {
+        let buttton = UIButton(type: .system)
+        buttton.setTitle("Prev", for: .normal)
+        buttton.translatesAutoresizingMaskIntoConstraints = false
+        buttton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        buttton.setTitleColor(.gray, for: .normal)
+        buttton.addTarget(self, action: #selector(handlePrevius), for: .touchUpInside)
+        return buttton
+    }()
+    
+    private let nextButton: UIButton = {
+        let buttton = UIButton(type: .system)
+        buttton.setTitle("Next", for: .normal)
+        buttton.translatesAutoresizingMaskIntoConstraints = false
+        buttton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        buttton.setTitleColor(.mainPink, for: .normal)
+        buttton.addTarget(self, action: #selector(handleNext), for: .touchUpInside)
+        return buttton
+    }()
+    
+    @objc private func handleNext(){
+        let nextIndex = min(pageControl.currentPage + 1, pages.count - 1)
+        let indexPath = IndexPath(item: nextIndex, section: 0)
+        pageControl.currentPage = nextIndex
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
+    @objc private func handlePrevius(){
+        let nextIndex = max(pageControl.currentPage - 1, 0)
+        let indexPath = IndexPath(item: nextIndex, section: 0)
+        pageControl.currentPage = nextIndex
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
+    private lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = pages.count
+        pageControl.currentPageIndicatorTintColor = .mainPink
+        pageControl.pageIndicatorTintColor = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+        return pageControl
+    }()
+    
+    private func setupBottomControls(){
+        let bottonControlssStackView = UIStackView(arrangedSubviews: [previusButton,pageControl,nextButton])
+        bottonControlssStackView.distribution = .fillEqually
+        view.addSubview(bottonControlssStackView)
+        bottonControlssStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([bottonControlssStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor), bottonControlssStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor), bottonControlssStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor), bottonControlssStackView.heightAnchor.constraint(equalToConstant: 50)])
+    }
+    
+    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let x = targetContentOffset.pointee.x
+        
+        pageControl.currentPage = Int(x / view.frame.width)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .white
         collectionView.register(PageCell.self, forCellWithReuseIdentifier: "cellId")
         collectionView.isPagingEnabled = true
+        setupBottomControls()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
